8bb004abddcb77d6060eb7b5bc9d7978fcfb29d7
12tnewd-source <12.t.new.d@gmail.com>
2026-05-01 17:33:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,15 @@
-from fastapi import FastAPI, Request
-import requests
-import os
-
-app = FastAPI()
-
-LINE_ACCESS_TOKEN = os.getenv("LINE_ACCESS_TOKEN")
-
-@app.get("/")
-def root():
-    return {"msg": "ok"}
-
 @app.post("/callback")
 async def callback(request: Request):
     body = await request.json()
 
     for event in body["events"]:
+        if event["type"] != "message":
+            continue
+        if event["message"]["type"] != "text":
+            continue
+
         reply_token = event["replyToken"]
         user_msg = event["message"]["text"]
-
-        # とりあえずオウム返し
-        reply_msg = f"お前『{user_msg}』って言ったやろｗ"
 
         headers = {
             "Authorization": f"Bearer {LINE_ACCESS_TOKEN}",
@@ -28,9 +18,10 @@ async def callback(request: Request):
 
         data = {
             "replyToken": reply_token,
-            "messages": [
-                {"type": "text", "text": reply_msg}
-            ]
+            "messages": [{
+                "type": "text",
+                "text": "動いたで！"
+            }]
         }
 
         requests.post(
@@ -39,4 +30,4 @@ async def callback(request: Request):
             json=data
         )
 
-    return {"status": "ok"}
+    return "OK"
