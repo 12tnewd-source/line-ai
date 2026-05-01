ac987f5659b98ffe417e271507ad871cee90c688
12tnewd-source <12.t.new.d@gmail.com>
2026-05-01 16:46:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import requests
+import os
 
 app = FastAPI()
+
+LINE_ACCESS_TOKEN = os.getenv("LINE_ACCESS_TOKEN")
 
 @app.get("/")
 def root():
     return {"msg": "ok"}
 
 @app.post("/callback")
-async def callback():
+async def callback(request: Request):
+    body = await request.json()
+
+    for event in body["events"]:
+        reply_token = event["replyToken"]
+        user_msg = event["message"]["text"]
+
+        # とりあえずオウム返し
+        reply_msg = f"お前『{user_msg}』って言ったやろｗ"
+
+        headers = {
+            "Authorization": f"Bearer {LINE_ACCESS_TOKEN}",
+            "Content-Type": "application/json"
+        }
+
+        data = {
+            "replyToken": reply_token,
+            "messages": [
+                {"type": "text", "text": reply_msg}
+            ]
+        }
+
+        requests.post(
+            "https://api.line.me/v2/bot/message/reply",
+            headers=headers,
+            json=data
+        )
+
     return {"status": "ok"}
